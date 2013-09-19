acba923d4501aabcfdc0fa2526ea203ea550b9ec
Gioele Meoni <gioelemeoni@gmail.com>
2013-09-19 09:33:22 +0200

    re-organize package structure

---
+++ wifiLazooo/src/com/lazooo/wifi/android/application/SlidingContent.java
@@ -0,0 +1,27 @@
+package com.lazooo.wifi.android.application;
+
+import android.os.Bundle;
+import com.jeremyfeinstein.slidingmenu.example.R;
+
+
+public class SlidingContent extends BaseActivity {
+	
+	public SlidingContent() {
+		super(R.string.title_bar_content);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// set the Above View
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, new SampleListFragment())
+		.commit();
+		
+		setSlidingActionBarEnabled(false);
+	}
+
+}
