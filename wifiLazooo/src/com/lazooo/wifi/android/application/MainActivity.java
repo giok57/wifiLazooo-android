4640c8e9f7b7d615685216d1c33eaa7c73be7b07
litiales <litiales@gmail.com>
2013-09-30 17:14:44 +0200

    enabled menu

---
+++ wifiLazooo/src/com/lazooo/wifi/android/application/MainActivity.java
@@ -0,0 +1,98 @@
+package com.lazooo.wifi.android.application;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.widget.GridView;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+public class MainActivity extends FragmentActivity {
+
+    private SlidingMenu slidingMenu;
+
+    @Override
+    public void onCreate(Bundle savedBundle) {
+
+        super.onCreate(savedBundle);
+        setTitle("Gioele puzza");
+        setContentView(R.layout.test);
+
+        setupImageLoader();
+        setupSlidingMenu();
+
+    }
+
+    //definisce il comportamento che si ha quando viene spinto il tasto indietro
+    @Override
+    public void onBackPressed() {
+        if (slidingMenu.isMenuShowing()) {
+            slidingMenu.toggle();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //apre il menu se viene spinto il tasto menu
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            this.slidingMenu.toggle();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.slidingMenu.toggle();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupImageLoader() {
+
+        DisplayImageOptions displayImageOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisc(true).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .memoryCacheExtraOptions(480, 800)
+                .discCacheExtraOptions(480, 800, Bitmap.CompressFormat.JPEG, 75, null)
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .discCacheFileCount(100)
+                .defaultDisplayImageOptions(displayImageOptions)
+                .build();
+
+        ImageLoader.getInstance().init(config);
+
+    }
+
+    private void setupSlidingMenu() {
+
+        slidingMenu = new SlidingMenu(this);
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setShadowWidthRes(R.dimen.slidingmenu_shadow_width);
+        slidingMenu.setShadowWidth(R.drawable.slidingmenu_shadow);
+        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        slidingMenu.setFadeDegree(0.35f);
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        slidingMenu.setMenu(R.layout.slidingmenu);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
